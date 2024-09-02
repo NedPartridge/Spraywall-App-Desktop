a3cf44aef2fc128acab814648836dbbc1e344ea9
HappyElectron <ned.fpartridge@gmail.com>
2024-09-02 14:34:41 +1000

    buttons, contact us

--- SpraywallAppDesktop/Pages/SidePanel.xaml.cs
+++ SpraywallAppDesktop/Pages/SidePanel.xaml.cs
@@ -1,89 +1,129 @@
-namespace SpraywallAppDesktop.Pages
+namespace SpraywallAppDesktop.Pages;
+public partial class SidePanel : ContentView
 {
-    public partial class SidePanel : ContentView
+    public static readonly BindableProperty ButtonParameterProperty =
+        BindableProperty.Create(
+            nameof(ButtonParameter),
+            typeof(string),
+            typeof(SidePanel),
+            propertyChanged: OnButtonParameterChanged);
+
+    public string ButtonParameter
     {
-        public static readonly BindableProperty ButtonParameterProperty =
-            BindableProperty.Create(
-                nameof(ButtonParameter),
-                typeof(string),
-                typeof(SidePanel),
-                propertyChanged: OnButtonParameterChanged);
+        get => (string)GetValue(ButtonParameterProperty);
+        set => SetValue(ButtonParameterProperty, value);
+    }
 
-        public string ButtonParameter
+    public SidePanel()
+    {
+        InitializeComponent();
+    }
+
+    private static void OnButtonParameterChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (SidePanel)bindable;
+        control.UpdateSettingsButtonColor((string)newValue);
+    }
+
+
+    // Use colours to show the user which page they're on
+    private void UpdateSettingsButtonColor(string parameter)
+    {
+        if (parameter == "settings")
         {
-            get => (string)GetValue(ButtonParameterProperty);
-            set => SetValue(ButtonParameterProperty, value);
+            SettingsButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
+            SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+            AddWallButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+
+            SettingsButton.TextColor = Colors.White;
+            SpraywallAppButton.TextColor = Colors.Black;
+            AddWallButton.TextColor = Colors.Black;
         }
-
-        public SidePanel()
+        else if (parameter == "home")
         {
-            InitializeComponent();
+            SettingsButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+            SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
+            AddWallButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+
+            SettingsButton.TextColor = Colors.Black;
+            SpraywallAppButton.TextColor = Colors.White;
+            AddWallButton.TextColor = Colors.Black;
         }
-
-        private static void OnButtonParameterChanged(BindableObject bindable, object oldValue, object newValue)
+        else if (parameter == "add wall")
         {
-            var control = (SidePanel)bindable;
-            control.UpdateSettingsButtonColor((string)newValue);
+            SettingsButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+            SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+            AddWallButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
+
+            SettingsButton.TextColor = Colors.Black;
+            SpraywallAppButton.TextColor = Colors.Black;
+            AddWallButton.TextColor = Colors.White;
         }
+    }
 
+    // Navigate to home page
+    private async void OnSpraywallAppClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//" + nameof(Home));
+    }
 
-        // Use colours to show the user which page they're on
-        private void UpdateSettingsButtonColor(string parameter)
+    // Navigate to add wall page
+    private async void OnAddWallClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//" + nameof(AddWall));
+    }
+
+    // Navigate to settings page
+    private async void OnSettingsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//" + nameof(Settings));
+    }
+
+    // Open contact mailto
+    private async void OnContactUsClicked(object sender, EventArgs e)
+    {
+        try
         {
-            if (parameter == "settings")
+            var message = new EmailMessage
             {
-                SettingsButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
-                SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
-                AddWallButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
+                Subject = "Contact Us",
+                Body = "Please enter your message here...",
+                To = new List<string> { "support@spraywallapp.com" }
+            };
 
-                SettingsButton.TextColor = Colors.White;
-                SpraywallAppButton.TextColor = Colors.Black;
-                AddWallButton.TextColor = Colors.Black;
-            }
-            else if (parameter == "home")
+            await Email.Default.ComposeAsync(message);
+        }
+        catch (FeatureNotSupportedException fbsEx)
+        {
+            // Email is not supported on this device
+            var parentPage = this.GetParentPage();
+            if (parentPage != null)
             {
-                SettingsButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
-                SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
-                AddWallButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
-
-                SettingsButton.TextColor = Colors.Black;
-                SpraywallAppButton.TextColor = Colors.White;
-                AddWallButton.TextColor = Colors.Black;
+                await parentPage.DisplayAlert("Error", "Email is not supported on this device", "OK");
             }
-            else if (parameter == "add wall")
+        }
+        catch (Exception ex)
+        {
+            // Some other error occurred
+            var parentPage = this.GetParentPage();
+            if (parentPage != null)
             {
-                SettingsButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
-                SpraywallAppButton.BackgroundColor = (Color)Application.Current.Resources["FernGreen"];
-                AddWallButton.BackgroundColor = (Color)Application.Current.Resources["ForestGreen"];
-
-                SettingsButton.TextColor = Colors.Black;
-                SpraywallAppButton.TextColor = Colors.Black;
-                AddWallButton.TextColor = Colors.White;
+                await parentPage.DisplayAlert("Error", "Unknown cause", "OK");
             }
         }
+    }
 
-        // Navigate to home page
-        private async void OnSpraywallAppClicked(object sender, EventArgs e)
+    private Page GetParentPage()
+    {
+        Element parent = this;
+        while (parent != null)
         {
-            await Shell.Current.GoToAsync("//" + nameof(Home));
+            if (parent is Page page)
+            {
+                return page;
+            }
+            parent = parent.Parent;
         }
-
-        // Navigate to add wall page
-        private async void OnAddWallClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//" + nameof(AddWall));
-        }
-
-        // Navigate to settings page
-        private async void OnSettingsClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//" + nameof(Settings));
-        }
-
-        // Open contact mailto
-        private void OnContactUsClicked(object sender, EventArgs e)
-        {
-            
-        }
+        return null;
     }
 }
